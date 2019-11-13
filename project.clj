e47e203bc00344f43028dcdade1986d2bbbb23cd
Rajesh Shah <raj@12nines.com>
2019-11-13 07:28:34 +0000

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(def jetty-version "9.4.0.v20161208")
-(defproject cc.qbits/jet "0.7.11"
+(def jetty-version "9.4.22.v20191022")
+(defproject com.twelvenines/jet "0.0.1"
   :description "Jetty9 ring server adapter with WebSocket support"
   :url "https://github.com/mpenet/jet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.5.527"]
                  [org.eclipse.jetty/jetty-server ~jetty-version]
                  [org.eclipse.jetty.http2/http2-server ~jetty-version]
                  [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
@@ -18,7 +18,7 @@
                  ;; [org.eclipse.jetty.http2/http2-http-client-transport ~jetty-version]
                  ;; [org.eclipse.jetty.http2/http2-client ~jetty-version]
                  ;; [org.eclipse.jetty/jetty-alpn-client ~jetty-version]
-                 [cheshire "5.5.0"]]
+                 [cheshire "5.9.0"]]
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
@@ -26,12 +26,12 @@
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
   :profiles {:master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
-             :dev  {:dependencies [[ring/ring-core "1.3.0"
+             :dev  {:dependencies [[ring/ring-core "1.7.1"
                                     :exclusions [javax.servlet/servlet-api]]
-                                   [ring/ring-servlet "1.3.0"
+                                   [ring/ring-servlet "1.7.1"
                                     :exclusions [javax.servlet/servlet-api]]
-                                   [codox "0.8.10"]
-                                   [org.slf4j/slf4j-log4j12 "1.7.3"]]}}
+                                   [codox "0.10.7"]
+                                   [org.slf4j/slf4j-log4j12 "1.7.29"]]}}
   :jar-exclusions [#"log4j.properties"]
   :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.6:dev,1.7:dev,master"]}
   :codox {:src-dir-uri "https://github.com/mpenet/jet/blob/master/"
