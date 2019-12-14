ed3cce37455ef58a02fee0ff1808c8b2e4d2cc44
Rajesh Shah <raj@12nines.com>
2019-12-14 13:50:00 +0000

    updated to use latest version of core.async

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.5.527"]
+                 [org.clojure/core.async "0.6.532"]
                  [org.eclipse.jetty/jetty-server ~jetty-version]
                  [org.eclipse.jetty.http2/http2-server ~jetty-version]
                  [org.eclipse.jetty.websocket/websocket-server ~jetty-version]
